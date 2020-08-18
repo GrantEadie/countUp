34c968a597debc1132a08926ea0e11d351c87149
Alex Goodwin <hagoodwi@gmail.com>
2020-08-18 10:39:40 -0700

    end proejct with bugs still
    Co-authored-by: Grant Eadie <grantleadie@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   $("#form0ne").submit(function(event) {
   event.preventDefault();
-  let number1 = parseInt($("input#number1").val());
-  let number2 = parseInt($("input#number2").val());
-  
-  for (let i = 0; i <= number1; i += number2) {
-    alert(i);
-  }
 
+    let number1 = $("input#number1").val();
+    let number2 = $("input#number2").val();
+    
+    if (number1 !== "" || number2 !== "" || number1 < 0 || number2 < 0) {
+      number1 = parseInt(number1);
+      number2 = parseInt(number2);
+        for (let i = 0; i <= number1; i += number2) {
+          alert(`Output: ${i}`);
+        }
+    } else {
+      alert("enter a number");
+    }
   });
 });
+
+//number1.
+//number1.isInteger() || number2.isInteger()
